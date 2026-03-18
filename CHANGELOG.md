146528b2421f9bc92ebc9d897fbd255556d844cf
ympax14 <nathan.ghys@etu.univ-amu.fr>
2026-03-18 12:00:05 +0100

    feat: Ajout d'options dans la MenuBar et Onglets
    Voir #CHANGELOG.md section 0.2.0-dev

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,3 +6,10 @@ Toutes les modifications sont documentées selon [Semantic Versioning](https://s
 ### Added
 - Page de Connexion (LoginDialog) avec email/identifiant et mot de passe
 - Validation simple des champs
+
+## [0.1.0-dev] - 2026-03-17
+### Added
+- Options "Quitter" et "Nouvelle Facture" dans la QMenuBar
+- Icons/Images dans le fichier ressources.qrc
+- Onglets "Factures", "Estimations", "Comparateur" dans un QTabWidget de la QMainWindow
+- Un QMdiArea pour l'onglet "Factures"
--- src/EnerSense/EnerSenseWindow.hpp
+++ src/EnerSense/EnerSenseWindow.hpp
@@ -2,15 +2,32 @@
 #define ENERSENSEWINDOW_H
 
 #include <QMainWindow>
+#include <QMenuBar>
+#include <QMenu>
+#include <QTabWidget>
+#include <QMdiArea>
+#include <QVBoxLayout>
+#include <QApplication>
 
 class EnerSenseWindow : public QMainWindow {
     Q_OBJECT
 private:
+    QMenu *fileMenu;
+    QAction *newBillAction, *addBillAction, *quitAction;
 
+    QTabWidget *tabWidget;
+
+    QWidget *billsPage;
+    QVBoxLayout *billsPageLayout;
+    QMdiArea *billsArea;
+
+    QWidget *estimatePage;
+
+    QWidget *offersComparatorPage;
 public:
     static const int WINDOW_WIDTH = 1280, WINDOW_HEIGHT = 720;
 
-    explicit EnerSenseWindow(QWidget *parent = nullptr);
+    explicit EnerSenseWindow(QWidget *parent = nullptr, QApplication *app = nullptr);
 
 signals:
 };
